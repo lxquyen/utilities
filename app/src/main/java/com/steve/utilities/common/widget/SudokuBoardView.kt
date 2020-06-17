ae5b95eb90fbc27a9e8b2b008e7f8b4c9a2df246
lxquyenx-home <lxquyen@gmail.com>
2020-06-18 00:47:42 +0700

    draw board

--- app/src/main/java/com/steve/utilities/common/widget/SudokuBoardView.kt
+++ app/src/main/java/com/steve/utilities/common/widget/SudokuBoardView.kt
@@ -1,26 +1,44 @@
 package com.steve.utilities.common.widget
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.Pair
+import android.view.MotionEvent
 import android.view.View
 import com.steve.utilities.R
 
 class SudokuBoardView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
-    private var lineColor = Color.BLACK
-
     private var cellWidth = 0f
     private var cellHeight = 0f
+
+    private var boardColor = 0
+    private var backgroundSelectedColor = 0
+
+    private var lineColorPrimary = 0
+    private var lineColorSecondary = 0
+
+    private var lineStrokePrimary = 0f
+    private var lineStrokeSecondary = 0f
 
     private val linePaint: Paint by lazy {
         return@lazy Paint(Paint.ANTI_ALIAS_FLAG)
             .apply {
-                color = lineColor
+                color = lineColorPrimary
             }
+    }
+    private val boardPaint: Paint by lazy {
+        return@lazy Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = boardColor
+        }
+    }
+    private val selectPaint: Paint by lazy {
+        return@lazy Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = Color.YELLOW
+        }
     }
 
     init {
@@ -30,7 +48,12 @@ class SudokuBoardView(context: Context?, attrs: AttributeSet?) : View(context, a
     private fun setupAttrs(attrs: AttributeSet?) {
         context.obtainStyledAttributes(attrs, R.styleable.SudokuBoardView)
             .let {
-                lineColor = it.getColor(R.styleable.SudokuBoardView_lineColor, Color.BLACK)
+                boardColor = it.getColor(R.styleable.SudokuBoardView_boardColor, Color.WHITE)
+                backgroundSelectedColor = it.getColor(R.styleable.SudokuBoardView_backgroundSelectedColor, Color.YELLOW)
+                lineColorPrimary = it.getColor(R.styleable.SudokuBoardView_lineColorPrimary, Color.BLUE)
+                lineColorSecondary = it.getColor(R.styleable.SudokuBoardView_lineColorSecondary, Color.BLACK)
+                lineStrokePrimary = it.getDimension(R.styleable.SudokuBoardView_lineStrokePrimary, 10f)
+                lineStrokeSecondary = it.getDimension(R.styleable.SudokuBoardView_lineStrokeSecondary, 5f)
                 return@let it
             }
             .recycle()
@@ -48,17 +71,46 @@ class SudokuBoardView(context: Context?, attrs: AttributeSet?) : View(context, a
         super.onMeasure(MeasureSpec.makeMeasureSpec(min, MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(min, MeasureSpec.EXACTLY))
     }
 
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return super.onTouchEvent(event)
+    }
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        //Draw vertical lines
-        (0..9).map {
+        //Draw boardBackground
+        canvas?.drawRect(cellWidth * 3, 0f, cellWidth * 6, cellHeight * 3, boardPaint)
+        canvas?.drawRect(0f, cellWidth * 3, cellWidth * 3, cellHeight * 6, boardPaint)
+        canvas?.drawRect(cellWidth * 6, cellWidth * 3, cellWidth * 12, cellHeight * 6, boardPaint)
+        canvas?.drawRect(cellWidth * 3, cellWidth * 6, cellWidth * 6, cellHeight * 12, boardPaint)
+
+        //Draw lines
+        (0..9).forEach {
             val x = it * cellWidth + paddingLeft
             val y = it * cellHeight + paddingTop
-            return@map Pair.create(x, y)
-        }.forEach {
-            canvas?.drawLine(it, paddingTop.toFloat(), it, height.toFloat(), linePaint)
+            canvas?.save()
+
+            canvas?.restore()
+
+            linePaint.apply {
+                color = if (it % 3 == 0) lineColorPrimary else lineColorSecondary
+                strokeWidth = if (it % 3 == 0) lineStrokePrimary else lineStrokeSecondary
+            }
+
+            canvas?.drawLine(x, 0f, x, height.toFloat(), linePaint)
+            canvas?.drawLine(0f, y, width.toFloat(), y, linePaint)
+
+
         }
+
+        //Draw Selected background
+        val bgSelectedX = cellWidth * 0f + lineStrokeSecondary
+        val bgSelectedY = cellHeight * 0f + lineStrokeSecondary
+        val bgSelectedWidth = cellWidth - lineStrokeSecondary
+        val bgSelectedHeight = cellHeight - lineStrokeSecondary
+
+        canvas?.drawRect(bgSelectedX, bgSelectedY, bgSelectedWidth, bgSelectedHeight, selectPaint)
 
     }
 
